f1b6c69a6be3ff74a998595390136f8c9665a167
Daniel Goloubev <danielgoloubev@gmail.com>
2018-02-25 16:18:46 +0100

    Completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
 var sumAll = function() {
-
+  let ind = Math.min(arguments[0],arguments[1]);
+  let len = Math.max(arguments[0],arguments[1]);
+  let sum = 0;
+  if ( ind > 0 && len > 0 && typeof arguments[0] === 'number' && typeof arguments[1] === 'number') {
+    for(ind; ind <= len; ind++){
+      sum += ind;
+    }
+  } else {
+    sum = "ERROR";
+  }
+  return sum;
 }
 
 module.exports = sumAll
