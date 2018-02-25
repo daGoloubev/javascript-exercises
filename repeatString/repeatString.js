bb02a24fe6694f5cd0f8a0748cab7287968caf0e
Daniel Goloubev <danielgoloubev@gmail.com>
2018-02-25 14:44:54 +0100

    Completed repeat string.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-var repeatString = function() {
-
+var repeatString = function(string, times) {
+  let r = "";
+  while(times > 0){
+     r += string;
+     times = times - 1;
+  }
+  return times < 0 ? 'ERROR': r;
 }
 
 module.exports = repeatString
