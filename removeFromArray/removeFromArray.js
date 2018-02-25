ef9d65b36611be92e472f38941a804d5df3cadd4
Daniel Goloubev <danielgoloubev@gmail.com>
2018-02-25 15:56:27 +0100

    completed remove from array.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 var removeFromArray = function() {
-
+  var arr = arguments[0];
+  for(var i = 1; i < arguments.length; i++){
+    var ix = arr.indexOf(arguments[i]);
+    if(ix > -1){
+      arr.splice(ix,1);
+    }
+  }
+  return arr;
 }
 
 module.exports = removeFromArray
