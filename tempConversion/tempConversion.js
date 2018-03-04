d63ddacabbcff0f0971a62a8093bee33be6b4cb8
Daniel Goloubev <danielgoloubev@gmail.com>
2018-03-04 10:34:45 +0100

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-var ftoc = function() {
-  
+function precisionRound(number, precision) {
+  var factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
+}
+var ftoc = function(f) {
+  return precisionRound((f - 32) * (5 / 9), 1);
 }
 
-var ctof = function() {
-  
+var ctof = function(c) {
+  return precisionRound((c / (5 / 9)) + 32, 1);
 }
 
 module.exports = {
