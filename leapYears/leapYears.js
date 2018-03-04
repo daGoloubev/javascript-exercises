f663ffc9a1d4bd81758936f7db6461b7a227544a
Daniel Goloubev <danielgoloubev@gmail.com>
2018-03-04 10:04:54 +0100

    leayYears compelete

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,22 @@
 var leapYears = function() {
-
+  var c = arguments[0];
+  if(c % 4 === 0) {
+    if(
+      ((c / 400) % 1).toFixed(1) === '1.0' ||
+      ((c / 400) % 1) === 0 ||
+      ((c / 400) % 1).toFixed(1) === '0.5'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if(c % 100 === 0 && c % 400 !== 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = leapYears
